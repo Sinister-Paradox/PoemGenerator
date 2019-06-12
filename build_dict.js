245d7edcb2c4e47a2b065026d79bac7db11559d5
Sinister-Paradox <alexdejong737@gmail.com>
2019-06-12 17:59:15 -0500

    This ain't working

--- build_dict.js
+++ build_dict.js
@@ -1,15 +1,5 @@
-const csv = require('csv-parser');  
 const fs = require('fs');
 
 markovData = {};
+startWords = {}
 
-fs.createReadStream('data.csv')
-  .pipe(csv())
-  .on('data', (row) => {
-    //Process Here
-    
-    
-  })
-  .on('end', () => {
-    console.log('CSV file successfully processed');
-  });
